7261023b7e66ea9b7fc2002e8782e785c41072f4
Simon Iversen <simon.iversen@protonmail.com>
2022-02-20 17:14:14 +0100

    Set up basic template for Phaser.
    I have decided to try out Phaser. Really like the setup using the same
    functions.

--- boids.js
+++ boids.js
@@ -1,3 +1,25 @@
+var game = new Phaser.Game(4*480, 4*320,
+                           Phaser.CANVAS,
+                           null,
+                           {preload: preload,
+                            create: create,
+                            update: update}
+                          );
+var ball;
 
-var canvas = document.getElementById("boidsCanvas");
-var ctx = canvas.getContext("2d");
+function preload() {
+    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    game.scale.pageAlignHorizontally = true;
+    game.scale.pageAlignVertically = true;
+    game.stage.backgroundColor = '#fef';
+    game.load.image('ball', 'imgs/ball.png');
+}
+
+function create() {
+    ball = game.add.sprite(50, 50, 'ball');
+}
+
+function update() {
+    ball.x += 1;
+    ball.y += 1;
+}
